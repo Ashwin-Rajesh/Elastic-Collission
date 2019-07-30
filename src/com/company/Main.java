efd907672437220a00a322d48af67b57b9dac739
Ashwin Rajesh <aswinrajesh1@gmail.com>
2019-07-30 20:35:07 +0530

    Implemented brute force algorithm, successfull barring some glitchy behaviour

---
+++ src/com/company/Main.java
@@ -0,0 +1,64 @@
+package com.company;
+
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.awt.*;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int num = 100;
+        particle prts[] = new particle[num];
+
+        //prts[1] = new particle(new double[]{0,0}, new double[]{0.1,0}, 10);
+        //prts[0] = new particle(new double[]{5, 0}, new double[]{-0.1,0}, 1);
+        for(int k = 0; k < num; k++)
+        {
+            Random rand = new Random();
+            double[] pos = new double[2];
+            double[] vel = new double[2];
+            pos[1] = ((double)rand.nextInt(200) - 100)/10;
+            pos[0] = ((double)rand.nextInt(200) - 100)/10;
+            vel[0] = ((double) rand.nextInt(10) - 5 )/ 10;
+            vel[1] = ((double) rand.nextInt(10) - 5 )/ 10;
+            int mass = 10 + rand.nextInt(20)/2;
+            prts[k] = new particle(pos, vel, mass, Math.sqrt(mass)/30);
+            StdOut.println(prts[k].toString());
+        }
+
+        prts[0] = new particle(new double[]{0,0}, new double[]{0.1,0}, 100, 2);
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(-10, 10);
+        StdDraw.setYscale(-10, 10);
+        StdDraw.setPenRadius(0.01);
+
+        for (int i = 0; true; i++)
+        {
+            //StdOut.println("====================");
+            StdDraw.clear();
+            for(int j = 0; j < num; j++)
+            {
+                StdDraw.setPenColor((j * 10) % 127 + 128,(j * 7) % 255,(j * 8) % 255);
+                prts[j].move(0.01);
+                for(int k = j + 1; k < num; k++)
+                {
+                    if(prts[j].isCollide(prts[k]))
+                    {
+                        StdOut.println(j + " collide " + k);
+                        prts[j].collide(prts[k]);
+                    }
+                }
+                //StdOut.println("------------------------");
+                //StdOut.println(" Point no : " + j);
+                //StdOut.println(prts[j].toString());
+                prts[j].draw();
+            }
+            StdDraw.show();
+            StdDraw.pause(1);
+        }
+    }
+}
---
+++ src/com/company/particle.java
@@ -0,0 +1,98 @@
+package com.company;
+
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+public class particle {
+	private double[] pos;			// Stores position(x, y components in array) of particle
+	private double[] vel;			// Stores velocity(x, y components in array) of particle
+	private double mass;				// Stores mass of particle
+	private double radius;				// Stores radius of particle
+
+	public particle(double[] pos, double[] vel, double mass, double radius)
+	{
+		if(pos == null || vel == null)
+			throw new IllegalArgumentException(" Null arguments passed");
+
+		if(pos.length != vel.length)
+			throw new IllegalArgumentException(" Position, velocity vectors do not have same dimnensions");
+
+		this.pos = pos.clone();
+		this.vel = vel.clone();
+		this.mass = mass;
+		this.radius = radius;
+	}
+
+	public void collide(particle that)
+	{
+		if(this.vel.length != that.vel.length)
+			throw new IllegalArgumentException(" Two particles are in different dimensional spaces");
+
+		double mass_diff = this.mass - that.mass;
+		double mass_sum  = this.mass + that.mass;
+		for(int i = 0; i < vel.length; i++)
+		{
+			this.vel[i] = ((2 * that.mass * that.vel[i]) + (mass_diff * this.vel[i]))/mass_sum;
+			that.vel[i] = -((2 * this.mass * this.vel[i]) - (mass_diff * that.vel[i]))/mass_sum;
+		}
+		while(this.isCollide(that))
+			that.move(0.1);
+	}
+
+	public void move(double dt)
+	{
+		for(int i = 0; i < pos.length; i++)
+		{
+			pos[i] += vel[i] * dt;
+			if (Math.abs(pos[i]) >= 10)
+				vel[i] *= -1;
+		}
+	}
+
+	public void draw()
+	{
+		if(pos.length > 2)
+			throw new IllegalArgumentException(" Only till 2 dimensions can be drawn as of now.");
+
+		if(pos.length == 1)
+			StdDraw.point(pos[0],0);
+		else
+			//StdDraw.point(pos[0], pos[1]);
+			StdDraw.filledCircle(pos[0], pos[1], radius);
+	}
+
+	public String toString()
+	{
+		String str = "";
+		str += " Dimension : " + pos.length + "\n";
+		str += " Mass      : " + mass + "\n";
+		str += " Radius    : " + radius + "\n";
+		str += " Position  : " + "\n";
+		for(Double d : pos)
+			str += d.toString() + " , ";
+		str += "\n";
+		str += " Velocity  : " + "\n";
+		for(Double v : vel)
+			str += v.toString() + " , ";
+		str += "\n";
+		return str;
+	}
+
+	public double distanceTo(particle that)
+	{
+		double dist = 0;
+		for(int i = 0; i < pos.length; i++)
+		{
+			dist += Math.pow(this.pos[i] - that.pos[i], 2);
+		}
+		dist = Math.sqrt(dist);
+		return dist;
+	}
+
+	public boolean isCollide(particle that)
+	{
+		if(this.distanceTo(that) <= this.radius + that.radius)
+			return true;
+		return false;
+	}
+}
